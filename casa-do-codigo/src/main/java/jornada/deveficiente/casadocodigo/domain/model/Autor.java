33910174a4e7eb3cae22456d6e4dbf3e0443f80b
Celso Ribeiro <celsofilho82@gmail.com>
2020-11-02 10:34:08 -0300

    refatorando a classe model Autor

--- casa-do-codigo/src/main/java/jornada/deveficiente/casadocodigo/domain/model/Autor.java
+++ casa-do-codigo/src/main/java/jornada/deveficiente/casadocodigo/domain/model/Autor.java
@@ -1,9 +1,14 @@
 package jornada.deveficiente.casadocodigo.domain.model;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Autor {
@@ -11,39 +16,29 @@ public class Autor {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	private String nome;
-	
-	private String email;
-	
-	private String descricao;
+	private @NotBlank String nome;
+	private @NotBlank @Email String email;
+	private @NotBlank @Size(max = 400) String descricao;
+	private LocalDateTime dataCriacao = LocalDateTime.now();
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
+	public Autor(@NotBlank String nome, @NotBlank @Email String email, @NotBlank @Size(max = 400) String descricao) {
 		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	@Override
+	public String toString() {
+		return "Autor [id=" + id + ", nome=" + nome + ", email=" + email + ", descricao=" + descricao + ", dataCriacao="
+				+ dataCriacao + "]";
 	}
 
 	public Long getId() {
 		return id;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
 	}
 
 }
