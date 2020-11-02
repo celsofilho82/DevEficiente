5fba95c3c254815840dd53bd0f70f8901bb5472a
Celso Ribeiro <celsofilho82@gmail.com>
2020-11-02 10:32:28 -0300

    criando o controller

---
+++ casa-do-codigo/src/main/java/jornada/deveficiente/casadocodigo/api/controller/AutoresController.java
@@ -0,0 +1,35 @@
+package jornada.deveficiente.casadocodigo.api.controller;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jornada.deveficiente.casadocodigo.domain.model.Autor;
+import jornada.deveficiente.casadocodigo.domain.request.AutorRequest;
+
+/*
+ * Controller 100% Coeso
+ * Controllers que utilizam todos os atributos.
+ * Carga Intriseca: 2 pontos
+ */
+
+@RestController
+public class AutoresController {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@PostMapping(value = "/autores")
+	@Transactional
+	public String create(@RequestBody @Valid AutorRequest request) {
+		Autor autor = request.toModel();
+		manager.persist(autor);
+		return autor.toString();
+	}
+}
+
