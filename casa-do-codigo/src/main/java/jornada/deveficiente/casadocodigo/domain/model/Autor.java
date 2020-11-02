fa1ac6f155739bc4a04a31f0304273e54dde81da
Celso Ribeiro <celsofilho82@gmail.com>
2020-11-02 09:10:41 -0300

    adicionando o model autor

---
+++ casa-do-codigo/src/main/java/jornada/deveficiente/casadocodigo/domain/model/Autor.java
@@ -0,0 +1,49 @@
+package jornada.deveficiente.casadocodigo.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Autor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String descricao;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+}
