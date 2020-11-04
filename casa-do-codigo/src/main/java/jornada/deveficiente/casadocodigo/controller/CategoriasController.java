53f3f524ca1180790363b9a797233c8f35e1bfa1
Celso Ribeiro <celsofilho82@gmail.com>
2020-11-04 09:05:01 -0300

    adicionado no categoria controller validação duplicidade nome

--- casa-do-codigo/src/main/java/jornada/deveficiente/casadocodigo/controller/CategoriasController.java
+++ casa-do-codigo/src/main/java/jornada/deveficiente/casadocodigo/controller/CategoriasController.java
@@ -5,19 +5,31 @@ import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jornada.deveficiente.casadocodigo.domain.model.Categoria;
 import jornada.deveficiente.casadocodigo.domain.request.CategoriaRequest;
+import jornada.deveficiente.casadocodigo.validation.NomeCategoraNaoPodeSerDuplicadoValidator;
 
 @RestController
 public class CategoriasController {
 
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Autowired
+	private NomeCategoraNaoPodeSerDuplicadoValidator nomeCategoraNaoPodeSerDuplicadoValidator;
+	
+	@InitBinder
+	public void init(WebDataBinder binder) {
+		binder.addValidators(nomeCategoraNaoPodeSerDuplicadoValidator);
+	}
 	
 	@PostMapping(value = "/categorias")
 	@Transactional
