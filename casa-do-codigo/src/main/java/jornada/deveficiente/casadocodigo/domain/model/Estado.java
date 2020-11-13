c0f46b9787fad9696e219a06ef1897c6573d94cf
Celso Ribeiro <celsofilho82@gmail.com>
2020-11-13 08:35:03 -0300

    add equals e hashcode para os models Pais e Estado

--- casa-do-codigo/src/main/java/jornada/deveficiente/casadocodigo/domain/model/Estado.java
+++ casa-do-codigo/src/main/java/jornada/deveficiente/casadocodigo/domain/model/Estado.java
@@ -22,7 +22,7 @@ public class Estado {
 
 	@ManyToOne
 	private Pais pais;
-	
+
 	@Deprecated
 	public Estado() {
 	}
@@ -43,6 +43,41 @@ public class Estado {
 
 	public Pais getPais() {
 		return pais;
+	}
+
+	public boolean pertenceAPais(Pais pais) {
+		return this.pais.equals(pais);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((pais == null) ? 0 : pais.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estado other = (Estado) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (pais == null) {
+			if (other.pais != null)
+				return false;
+		} else if (!pais.equals(other.pais))
+			return false;
+		return true;
 	}
 
 }
