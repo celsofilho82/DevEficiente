63258e36b744113ca947c3c5af30ee126ced13dd
Celso Ribeiro <celsofilho82@gmail.com>
2020-11-13 08:32:54 -0300

    add validador checar se o estado pertence ao pais informado

---
+++ casa-do-codigo/src/main/java/jornada/deveficiente/casadocodigo/validation/EstadoPertencePaisValidator.java
@@ -0,0 +1,42 @@
+package jornada.deveficiente.casadocodigo.validation;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import jornada.deveficiente.casadocodigo.domain.model.Estado;
+import jornada.deveficiente.casadocodigo.domain.model.Pais;
+import jornada.deveficiente.casadocodigo.domain.request.NovaCompraRequest;
+
+@Component
+public class EstadoPertencePaisValidator implements Validator {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		
+		return NovaCompraRequest.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		if (errors.hasErrors()) {
+			return;
+		}
+
+		NovaCompraRequest request = (NovaCompraRequest) target;
+		
+		Pais pais = manager.find(Pais.class, request.getIdpais());
+		Estado estado = manager.find(Estado.class, request.getIdEstado());
+		if (!estado.pertenceAPais(pais)) {
+			errors.reject("idEstado", null, "Este estado não pertence ao Pais selecionado");
+		}
+		
+	}
+
+}
