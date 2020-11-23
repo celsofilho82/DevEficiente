492b27e3a2a5107d24b973edc6681f4bd900d44f
Celso Ribeiro <celsofilho82@gmail.com>
2020-11-23 08:51:52 -0300

    setup do projeto

---
+++ mercado-livre/src/test/java/jornada/deveficiente/mercadolivre/MercadolivreApplicationTests.java
@@ -0,0 +1,13 @@
+package jornada.deveficiente.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
