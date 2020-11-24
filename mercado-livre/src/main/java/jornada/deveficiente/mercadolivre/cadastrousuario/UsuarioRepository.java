a9f9a1696ea7250dbdf33960d3fc29286643c54e
Celso Ribeiro <celsofilho82@gmail.com>
2020-11-23 22:30:05 -0300

    Criado a classe UsuarioRepository

---
+++ mercado-livre/src/main/java/jornada/deveficiente/mercadolivre/cadastrousuario/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package jornada.deveficiente.mercadolivre.cadastrousuario;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	Optional<Usuario> findByLogin(String login);
+
+}
