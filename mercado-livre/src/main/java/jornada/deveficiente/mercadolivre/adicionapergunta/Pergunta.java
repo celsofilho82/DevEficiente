d626691457eee7ec901502a7250d66bfd9303e8d
Celso Ribeiro <celsofilho82@gmail.com>
2021-01-17 12:50:38 -0300

    Adicionado equals e hashcode classes pergunta, categoria e usuario

--- mercado-livre/src/main/java/jornada/deveficiente/mercadolivre/adicionapergunta/Pergunta.java
+++ mercado-livre/src/main/java/jornada/deveficiente/mercadolivre/adicionapergunta/Pergunta.java
@@ -34,6 +34,10 @@ public class Pergunta {
 	@ManyToOne
 	private Usuario usuario;
 
+	@Deprecated
+	public Pergunta() {
+	}
+
 	public Pergunta(@NotBlank String titulo, @NotNull LocalDateTime dataCriacao, Produto produto, Usuario usuario) {
 		this.titulo = titulo;
 		this.dataCriacao = dataCriacao;
@@ -60,4 +64,42 @@ public class Pergunta {
 	public Usuario getUsuario() {
 		return usuario;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((produto == null) ? 0 : produto.hashCode());
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pergunta other = (Pergunta) obj;
+		if (produto == null) {
+			if (other.produto != null)
+				return false;
+		} else if (!produto.equals(other.produto))
+			return false;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
+			return false;
+		return true;
+	}
+
 }
